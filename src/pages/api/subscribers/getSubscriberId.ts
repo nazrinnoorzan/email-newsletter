70752b866844f1db2ca4beab082f72bd0b8456ed
Nazrin Noorzan <nazrinnoorzan@gmail.com>
2024-07-20 22:19:13 +0800

    test: check for POST api

--- src/pages/api/subscribers/getSubscriberId.ts
+++ src/pages/api/subscribers/getSubscriberId.ts
@@ -3,39 +3,44 @@ import { createCaller } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
 import { env } from "~/env";
 
-// interface ExtendedNextApiRequest extends NextApiRequest {
-//   body: {
-//     email: string;
-//   };
-// }
+interface ExtendedNextApiRequest extends NextApiRequest {
+  body: {
+    email: string;
+  };
+}
 
-const getSubscriberId = async (req: NextApiRequest, res: NextApiResponse) => {
+const getSubscriberId = async (
+  req: ExtendedNextApiRequest,
+  res: NextApiResponse,
+) => {
   const apiKey = req.headers["x-api-key"];
 
   if (apiKey !== env.X_API_KEY) {
     res.status(500).json({ message: "Invalid API key!" });
   }
 
-  // Create context and caller
-  // const ctx = await createTRPCContext({
-  //   req,
-  //   res,
-  //   info: {
-  //     isBatchCall: false,
-  //     calls: [],
-  //   },
-  // });
-  // const caller = createCaller(ctx);
-
-  try {
-    // const { email } = req.body;
-    // const user = await caller.subscriber.getSubscriberId({
-    //   email,
+  if (req.method === "POST") {
+    // Create context and caller
+    // const ctx = await createTRPCContext({
+    //   req,
+    //   res,
+    //   info: {
+    //     isBatchCall: false,
+    //     calls: [],
+    //   },
     // });
-    res.status(200).json({ isEmailExist: 1 });
-  } catch (cause) {
-    console.error(cause);
-    res.status(500).json({ isEmailExist: 0, memberId: null });
+    // const caller = createCaller(ctx);
+
+    try {
+      const { email } = req.body;
+      // const user = await caller.subscriber.getSubscriberId({
+      //   email,
+      // });
+      res.status(200).json({ isEmailExist: 1, memberId: email });
+    } catch (cause) {
+      console.error(cause);
+      res.status(500).json({ isEmailExist: 0, memberId: null });
+    }
   }
 };
 
